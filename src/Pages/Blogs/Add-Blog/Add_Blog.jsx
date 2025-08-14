e1b161b4980f0a65eefcb382bfe3f8338bae3586
Sunil Shelke <shelkesunil072@gmail.com>
2025-08-14 11:08:14 +0530

    new

--- src/Pages/Blogs/Add-Blog/Add_Blog.jsx
+++ src/Pages/Blogs/Add-Blog/Add_Blog.jsx
@@ -8,16 +8,38 @@ const Add_Blog = () => {
     <div className="add-blog-parent parent">
       <div className="add-blog-container container">
         <h3>Add Your Blog</h3>
-        <div className="left-side">
-          <form>
-            <input type="text" placeholder="Enter Blog Title" />
-            <MyEditor />
-            <input type="file"></input>
-            <button type="submit">Submit Blog</button>
-          </form>
-        </div>
-        <div className="right-side">
-          <h3>Preview Your Blog</h3>
+        <div className="blog-temp">
+          <div className="left-side">
+            <form>
+              <div className="input-group">
+                <label>Enter Blog Title</label>
+                <input type="text" placeholder="Enter Blog Title" />
+              </div>
+
+              <MyEditor />
+              <div className="input-group">
+                <label>Select Image</label>
+                <input type="file"></input>
+              </div>
+              <div className="input-group">
+                <label>Select Date</label>
+                <input type="date"></input>
+              </div>
+              <div className="input-group">
+                <label for="language">Select Language</label>
+                <select id="language" name="language">
+                  <option value="">Select</option>
+                  <option value="english">English</option>
+                  <option value="marathi">Marathi</option>
+                </select>
+              </div>
+
+              <button type="submit">Submit Blog-</button>
+            </form>
+          </div>
+          <div className="right-side">
+            <h4>Preview Your Blog</h4>
+          </div>
         </div>
       </div>
     </div>
